a6978415227367ab1d1c435e6211b6b4bf84eb19
andre-fajar-n <afajar@alterra.id>
2020-06-21 11:36:57 +0700

    edit docker

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ ssh-add -k ~/.ssh/id_rsa &&
 
 source ~/.profile
 echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
-docker stop wa_clone
-docker rm wa_clone
-docker rmi andresangfajar/be_wa_clone:latest
-docker run -d --name wa_clone -p 5050:5050 andresangfajar/be_wa_clone:latest
+sudo docker stop wa_clone
+sudo docker rm wa_clone
+sudo docker rmi andresangfajar/be_wa_clone:latest
+sudo docker run -d --name wa_clone -p 5050:5050 andresangfajar/be_wa_clone:latest
